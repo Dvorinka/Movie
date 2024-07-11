cf29ce449a183bbd6cbce7158bbc8739a452537e
Dvorinka <contact.dvorak@gmail.com>
2024-07-11 08:23:47 +0200

    changed img quality from tmdb original to w780 - better loading time

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,6 +3,6 @@ const omdbApiKey = '20e349a6';
 const upcomingMoviesUrl = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`;
 const popularMoviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
 const imageBaseUrl = 'https://image.tmdb.org/t/p/original';
-const imageBannerUrl = 'https://image.tmdb.org/t/p/w500';
+const imageBannerUrl = 'https://image.tmdb.org/t/p/w780';
 const baseApiUrl = 'https://api.themoviedb.org/3';
 const ytsBaseUrl = 'https://yts.mx/api/v2';
