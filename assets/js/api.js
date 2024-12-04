6308bfa700d569fe8f630b00b7da1a17f723dbc2
Tomáš Dvořák <150935816+Dvorinka@users.noreply.github.com>
2024-12-04 12:16:52 +0100

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,22 +1,14 @@
-const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-const omdbApiKey = process.env.NEXT_PUBLIC_OMDB_API_KEY;
-const upcomingMoviesUrl = process.env.NEXT_PUBLIC_UPCOMING_MOVIES_URL;
-const popularMoviesUrl = process.env.NEXT_PUBLIC_POPULAR_MOVIES_URL;
-const imageBaseUrl = process.env.NEXT_PUBLIC_IMAGE_BASE_URL;
-const imageBannerUrl = process.env.NEXT_PUBLIC_IMAGE_BANNER_URL;
-const baseApiUrl = process.env.NEXT_PUBLIC_BASE_API_URL;
-const ytsBaseUrl = process.env.NEXT_PUBLIC_YTS_BASE_URL;
-const accountID = process.env.NEXT_PUBLIC_ACCOUNT_ID;
-const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+const apiKey = NEXT_PUBLIC_API_KEY;
+const omdbApiKey = NEXT_PUBLIC_OMDB_API_KEY;
+const upcomingMoviesUrl = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&region=US&with_release_type=2&page=1`;
+const popularMoviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+const imageBaseUrl = NEXT_PUBLIC_IMAGE_BASE_URL;
+const imageBannerUrl = NEXT_PUBLIC_IMAGE_BANNER_URL;
+const baseApiUrl = NEXT_PUBLIC_BASE_API_URL;
+const ytsBaseUrl = NEXT_PUBLIC_YTS_BASE_URL;
+const accountID = NEXT_PUBLIC_ACCOUNT_ID;
+const accessToken = NEXT_PUBLIC_ACCESS_TOKEN;
 
 // Example usage
-console.log("TMDB API Key:", apiKey);
+console.log("API Key:", apiKey);
 console.log("OMDB API Key:", omdbApiKey);
-console.log("Upcoming Movies URL:", upcomingMoviesUrl);
-console.log("Popular Movies URL:", popularMoviesUrl);
-console.log("Image Base URL:", imageBaseUrl);
-console.log("Image Banner URL:", imageBannerUrl);
-console.log("Base API URL:", baseApiUrl);
-console.log("YTS Base URL:", ytsBaseUrl);
-console.log("Account ID:", accountID);
-console.log("Access Token:", accessToken);
