d10d72cb53ee24cd585286051701eda336292705
Tomáš Dvořák <150935816+Dvorinka@users.noreply.github.com>
2024-12-04 12:13:34 +0100

    Add files via upload

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,10 +1,22 @@
-const apiKey = '054582e9ee66adcbe911e0008aa482a8';
-const omdbApiKey = '20e349a6';
-const upcomingMoviesUrl = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&region=US&with_release_type=2&page=1`;
-const popularMoviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
-const imageBaseUrl = 'https://image.tmdb.org/t/p/original';
-const imageBannerUrl = 'https://image.tmdb.org/t/p/w780';
-const baseApiUrl = 'https://api.themoviedb.org/3';
-const ytsBaseUrl = 'https://yts.mx/api/v2';
-const accountID = '6578737f20ecaf00c699cdc4';
-const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNTQ1ODJlOWVlNjZhZGNiZTkxMWUwMDA4YWE0ODJhOCIsIm5iZiI6MTczMjM1MzY2NC44NTUyODQsInN1YiI6IjY1Nzg3MzdmMjBlY2FmMDBjNjk5Y2RjNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a5QAwVIb-gaZmD8jOwFsnzVr8wiSzLv0oXfE5ZLMnHE';
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+const omdbApiKey = process.env.NEXT_PUBLIC_OMDB_API_KEY;
+const upcomingMoviesUrl = process.env.NEXT_PUBLIC_UPCOMING_MOVIES_URL;
+const popularMoviesUrl = process.env.NEXT_PUBLIC_POPULAR_MOVIES_URL;
+const imageBaseUrl = process.env.NEXT_PUBLIC_IMAGE_BASE_URL;
+const imageBannerUrl = process.env.NEXT_PUBLIC_IMAGE_BANNER_URL;
+const baseApiUrl = process.env.NEXT_PUBLIC_BASE_API_URL;
+const ytsBaseUrl = process.env.NEXT_PUBLIC_YTS_BASE_URL;
+const accountID = process.env.NEXT_PUBLIC_ACCOUNT_ID;
+const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+
+// Example usage
+console.log("TMDB API Key:", apiKey);
+console.log("OMDB API Key:", omdbApiKey);
+console.log("Upcoming Movies URL:", upcomingMoviesUrl);
+console.log("Popular Movies URL:", popularMoviesUrl);
+console.log("Image Base URL:", imageBaseUrl);
+console.log("Image Banner URL:", imageBannerUrl);
+console.log("Base API URL:", baseApiUrl);
+console.log("YTS Base URL:", ytsBaseUrl);
+console.log("Account ID:", accountID);
+console.log("Access Token:", accessToken);
